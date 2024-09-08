5ba69f71bcc6a894166e25cddd3c00ddc1f982d2
Farhan <terfender@gmail.com>
2024-09-08 07:29:06 +0400

    added shorthandAddress func

--- src/server/utilities.js
+++ src/server/utilities.js
@@ -2,6 +2,17 @@ const currentUnixTimestamp = () => {
   return Math.floor(Date.now() / 1000);
 };
 
+const shorthandAddress = (address, length = 4) => {
+  if (!address) {
+    return "";
+  }
+  return `${address.substr(0, length)}...${address.substr(
+    address.length - length,
+    length
+  )}`;
+};
+
 module.exports = {
-  currentUnixTimestamp
+  currentUnixTimestamp,
+  shorthandAddress,
 };
